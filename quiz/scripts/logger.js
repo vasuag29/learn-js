efa2ad154533ede06a4b0267ca9b0e9f05cf5ae4
Vasu Agarwal <vasu@Vasus-MacBook-Pro.local>
2024-01-30 15:11:53 -0500

    Class activity

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,17 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg() {
+    console.log("error message", this.errMsg)
+}
+
+const o1 = {
+    errMsg: "o1",
+    logMsg: logMsg
+}
+
+const o2 = {
+    errMsg:"o2",
+    logMsg: logMsg
+}
+
+o1.logMsg()
+o2.logMsg()

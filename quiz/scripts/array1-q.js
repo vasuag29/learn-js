0a35d0b30cb66d430422380051da204f954d9f24
Vasu Agarwal <vasu@Vasus-MacBook-Pro.local>
2024-02-01 19:22:02 -0500

    Completed class activities

--- quiz/scripts/array1-q.js
+++ quiz/scripts/array1-q.js
@@ -14,14 +14,22 @@ window.onload = function() {
               // the display is regenerated every time a search term is entered.
               list.innerHTML = '';
 
+              myHistory.push(inp.value);
+              let myHistoryCopy = Array.from(myHistory);
+              myHistoryCopy.sort(function(a,b){
+                return a.length - b.length
+              });
+
               // loop through the sorted array, and display all the search terms in the list
               for (const itemText of myHistoryCopy) {
-                
+                const listItem = document.createElement("li");
+                listItem.textContent = itemText;
+                list.appendChild(listItem);
               }
 
               // If the array length is 5 or more, remove the oldest search term
               if (myHistory.length >= MAX_HISTORY) {
-                
+                myHistory.shift();
               }
 
               // empty the search input and focus it, ready for the next term to be entered
